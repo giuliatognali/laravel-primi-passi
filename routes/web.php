b07429bce094a32addc43f90b38534e64a2de48b
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-02 10:35:23 +0200

    $data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+        'subtitle' => 'Impariamo Laravel',  
+        'students' => [
+            'Pippo',
+            'Pluto'
+        ]
+    ];
+    return view('home', $data);
 });
